e7b444e3cacc06b6ad118545b70da509ffb4d6af
Javier Bonilla <bjavier@umich.edu>
2024-08-14 15:10:16 -0400

    added gpt.find to search for recent keywords in previous conversations

--- src/extension.ts
+++ src/extension.ts
@@ -7,6 +7,9 @@ import * as path from 'path';
 import * as os from 'os';
 import axios from 'axios';
 import {config} from './config';
+import readline from 'readline';
+
+
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -17,6 +20,7 @@ export function activate(context: vscode.ExtensionContext) {
 	const location = config.location
 	
 	const filePath1 = path.join(homeDir, `${location}/${crntYear}/2024-${crntMonth}`);
+	const filePathFind = path.join(homeDir, `${location}/${crntYear}`);
 	if (!fs.existsSync(filePath1)) {
 		// If it doesn't exist, create it
 		fs.mkdirSync(filePath1, {recursive: true});
@@ -30,6 +34,20 @@ export function activate(context: vscode.ExtensionContext) {
 		const fileName = generateTimestampedFileName();
 		const filePath = path.join(directory, fileName);
 		const message = `role: ${config.role}\nmodel: ${config.model}\n## Question:\n---------\n`;
+	
+		try {
+			fs.writeFileSync(filePath, message, { flag: 'a' });
+			vscode.window.showInformationMessage(`File created and content written: ${fileName}`);
+		} catch (error) {
+			vscode.window.showErrorMessage('Failed to create and write to the file');
+		}
+	
+		return filePath;
+	}
+	function createAndWriteToFile2(directory: string): string {//creates file using generated timestamp, and writes our info into file
+		const fileName = generateTimestampedFileName();
+		const filePath = path.join(directory, fileName);
+		const message = `Key-words: `;
 	
 		try {
 			fs.writeFileSync(filePath, message, { flag: 'a' });
@@ -58,7 +76,7 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 	
 
-	let sendToChatGPTCommand = vscode.commands.registerCommand('cligpt.send', async () => {
+	let sendToChatGPTCommand = vscode.commands.registerCommand('cligpt.push', async () => {
         const editor = vscode.window.activeTextEditor;
         if (editor) {
             const document = editor.document;
@@ -87,7 +105,7 @@ export function activate(context: vscode.ExtensionContext) {
 		if(editor){
 			const document = editor.document;
 			let content:string = document.getText();
-			const summary = await sendToChatGPT(content + "give me a 1 sentence summary of this conversation, I want you to make it as short a sentence as possible while still encapsolating the conversation. No more than 25 words but ideally less, and it doesnt have to be a pretty, complete sentence as long as I can understand the gist. Don't include me asking you to summarize this conversation as part of your summary of this conversation");
+			const summary = await sendToChatGPT(content + "In one line give me 2 to 6 key words seperated by a comma with a space on both sides ie. 'word1 , word2' that summarize this conversation. Don't include me asking you to summarize this conversation as part of your summary of this conversation or the role or model first listed in this conversation or any of the headers themselves");//"give me a 1 sentence summary of this conversation, I want you to make it as short a sentence as possible while still encapsolating the conversation. No more than 25 words but ideally less, and it doesnt have to be a pretty, complete sentence as long as I can understand the gist. Don't include me asking you to summarize this conversation as part of your summary of this conversation"
 			if (summary){
 				editor.edit(editBuilder => {
 					var totalLines = editor.document.lineCount;
@@ -112,6 +130,79 @@ export function activate(context: vscode.ExtensionContext) {
 			vscode.window.showErrorMessage('No response.');	
 		}
 	});
+	let GPTfind = vscode.commands.registerCommand('cligpt.find', async () =>{
+		if (!fs.existsSync(filePath1)) {
+			// If it doesn't exist, create it
+			fs.mkdirSync(filePath1, {recursive: true});
+		}
+		const gptfile = createAndWriteToFile2(filePathFind);
+		openFileInVSCode(gptfile);
+	});
+	let GPTsend = vscode.commands.registerCommand('cligpt.send', async () => {
+		const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const document = editor.document;
+            let content:string = document.getText();
+			iterateFolder(filePath1, content, 0, "");
+		}
+	});
+	function iterateFolder(dirPath: string, content: string, best_score: number, best_file: string): void {
+		//let best_score: number = -1;
+		//let best_file: string = "";
+		let num = 0;
+		fs.readdirSync(dirPath).forEach((file) => {
+			num += 1;
+			const fullPath = path.join(dirPath, file);
+			const stats = fs.statSync(fullPath);
+			
+			if (stats.isFile()) {
+				let crnt_score: number = fileScore(extractLastLine(fullPath), content);
+				if(crnt_score > best_score){
+					best_score = crnt_score
+					best_file = fullPath;
+				}
+			}
+			// else if(stats.isDirectory()){
+			// 	iterateFolder(fullPath, content, best_score, best_file);//recursive through directories
+			// }
+		});
+		console.log(num);
+		const editor = vscode.window.activeTextEditor;
+        if (editor) {
+			// const failPath = path.join(dirPath, "FailFile.md"); 
+			// openFileInVSCode(failPath);
+			const filePath = editor.document.uri.fsPath;
+			fs.unlink(filePath, (err) => {
+				if (err) {
+					// Couldn't delete the file for some reason
+					vscode.window.showErrorMessage(`Failed to delete ${path.basename(filePath)}`);
+					console.error(err);
+				}
+			});
+		}
+		openFileInVSCode(best_file);
+	}
+	function fileScore(summary: string, content: string){
+		let score: number = 0;
+		let words: string[] = summary.split(" ");
+		let words2: string[] = content.split(" ");
+		for (let word of words) {
+			for (let word2 of words2){
+				let word3 = word2 + ',';
+				if (word.toLowerCase() == word2.toLowerCase()){
+					score += 1;
+				}
+				else if(word.toLowerCase() == word3.toLowerCase()){
+					score += 1;
+				}
+			}
+		}
+		return score;
+	}
+	function extractLastLine(filePath: string){
+		const lines = fs.readFileSync(filePath).toString().split('\n');
+   		return lines.length > 0 ? lines[lines.length - 1] : '';
+	}
          
 	async function sendToChatGPT(content: string): Promise<string | null> {
 		const apiKey: string = config.apiKey;
@@ -164,7 +255,7 @@ export function activate(context: vscode.ExtensionContext) {
 		//vscode.window.showInformationMessage('Hello World from cligpt!');
 	});
 
-	context.subscriptions.push(createAndOpenFile, sendToChatGPTCommand, GPTfinish, GPTresume);
+	context.subscriptions.push(createAndOpenFile, sendToChatGPTCommand, GPTfinish, GPTresume, GPTfind, GPTsend);
 }
 
 // This method is called when your extension is deactivated
@@ -172,3 +263,5 @@ export function deactivate() {}
 // Take code from listdir to open a new file with name based on time and date. 
 // Store name of file being used that well have to give to cligpt, so when we then input text into the file and ask the question it
 // will take that question and run in on gpt.
+
+
